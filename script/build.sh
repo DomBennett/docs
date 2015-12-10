564439857b45293cb1ffd9348e15a786118df866
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-12-10 17:56:59 +0000

    travis attempt 16

--- script/build.sh
+++ script/build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # copy contents to _docs_repo
+shopt -s extglob
+shopt -s dotglob
 mkdir _docs_repo
-cp -R ./* _docs_repo/
+mv !(_docs_repo) _docs_repo
 # clone FGE into cwd
-git clone https://github.com/dombennett/fge.git ./
+git clone https://github.com/dombennett/fge.git
+mv _docs_repo fge/
+cd fge
 # build with python
 python script/build.py
 # build with jekyll
