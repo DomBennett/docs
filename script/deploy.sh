d4d38a3538a4f017a445a085d91e426fdc244b6e
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-12-10 11:46:18 +0000

    travis attempt 5

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,5 +1,4 @@
 git clone https://github.com/dombennett/fge.git ./
 git clone https://github.com/dombennett/docs.git _docs_repo/
-python script/build.py
 jekyll build
 htmlproof ./_site --only-4xx --disable-external
