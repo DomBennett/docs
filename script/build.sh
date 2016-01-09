d7f1e967b792e2292834665f6855043ce5755de4
Dominic Bennett <dominic.john.bennett@gmail.com>
2016-01-09 11:49:03 +0000

    Created copy docs script

--- script/build.sh
+++ script/build.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-# copy contents to _docs_repo
-shopt -s extglob
-shopt -s dotglob
-mkdir _docs_repo
-mv !(_docs_repo) _docs_repo
 # clone FGE into cwd
-git clone https://github.com/dombennett/fge.git
+git clone https://github.com/BES-QSIG/fge.git
 mv _docs_repo fge/
 # prebuild
-ruby script/prebuild.rb
 (cd fge; ruby script/prebuild.rb)
 # build with jekyll
 jekyll build -s fge
